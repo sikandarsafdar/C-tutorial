66f2c1ae6bd7e6d8b3ea518cc3169720953417ad
zubair-saif <zubairsaif700@gmail.com>
2022-02-18 21:42:23 +0500

    change in digit

--- tut9.cpp
+++ tut9.cpp
@@ -9,21 +9,21 @@ int main()
     cout << "Tell me your age" << endl;
     cin >> age;
 
-    // 1. Selection control structure: If else-if else ladder
-    // if((age<18) && (age>0)){
-    //     cout<<"You can not come to my party"<<endl;
-    // }
-    // else if(age==18){
-    //     cout<<"You are a kid and you will get a kid pass to the party"<<endl;
-    // }
-    // else if(age<1){
-    //     cout<<"You are not yet born"<<endl;
-    // }
-    // else{
-    //     cout<<"You can come to the party"<<endl;
-    // }
+    1. selection control structure: If else-if else ladder
+    if((age<18) && (age>0)){
+        cout<<"You can not come to my party"<<endl;
+    }
+    else if(age==18){
+        cout<<"You are a kid and you will get a kid pass to the party"<<endl;
+    }
+    else if(age<1){
+        cout<<"You are not yet born"<<endl;
+    }
+    else{
+        cout<<"You can come to the party"<<endl;
+    }
 
-    // 2. Selection control structure: Switch Case statements
+    2. Selection control structure: Switch Case statements
     switch (age)
     {
     case 18:
