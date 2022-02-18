81b767022cf525a418a8910a5bc38626b970f881
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-02-18 23:01:02 +0500

    call by reference and value

--- tut16.cpp
+++ tut16.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a,int b){
-int c = a + b;
-return c;
-}
-int swap(int a,int b){// temp a b
-int temp =a;          // 4   4  5
-a = b;                // 4   5  5
-b = temp;             // 4   5  4 
+int sum(int a, int b)
+{
+    int c = a + b;
+    return c;
 }
 
-int main (){
-    int a=4 ,b=5;
-    //cout <<"The sum of 4 and 5 is"<<sum(a,b);
-    cout<<"The value of a is"<<a<<"and the value of b is"<<swap(a, b)<<endl; 
-     return 0; 
+// Call by reference using pointers
+int swapPointer(int *a, int *b)
+{                 // temp a b
+    int temp = *a; // 4   4  5
+    *a = *b;        // 4   5  5
+    *b = temp;     // 4   5 4
+}
+
+// Call by reference using C++ reference Variables
+int &swapReferenceVar(int &a, int &b)
+{                 // temp a b
+    int temp = a; // 4   4  5
+    a = b;        // 4   5  5
+    b = temp;     // 4   5  4
+    return a;
+}
+int main()
+{
+    int x = 4, y = 5;
+    // cout <<"The sum of 4 and 5 is"<<sum(a,b);
+    cout << "The value of x is" << x << "and the value of y is" << y << endl;
+    // swap(x, y);// This will not swap a and b
+    // swapPointer(&x, &y);// This will swap a and b using pointer reference
+    swapReferenceVar(x, y) = 766; // This will swap a and b using reference variables
+    cout << "The value of x is" << x << "and the value of y is" <<y<< endl;
+       return 0;
 }
