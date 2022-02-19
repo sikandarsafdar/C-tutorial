0ba35b605d03cf0fd0d35ecd957bbfe2ff71a72d
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-02-19 15:34:42 +0500

    inline function

---
+++ tut17.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+inline int product(int a, int b)
+{
+    return a * b;
+}
+int main()
+{
+    int a, b;
+    cout << "Enter the value a and b" << endl;
+    cin >> a >> b;
+    cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   cout << "The product of a and b is" << product(a, b) << endl;
+   
+      system("pause");
+       return 0;
+}
