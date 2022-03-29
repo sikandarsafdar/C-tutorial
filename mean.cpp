8e108455efffa49fe5b2594f9afbc82a162dabd3
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-03-29 18:40:06 +0500

    mean

---
+++ mean.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e;
+    a = 51.0;
+    b = 20.0;
+    c = 10.0;
+    d = 5.0;
+    e = 15.0;
+       float mean;
+       mean= (a+b+c+d+e)/5;
+     cout<<"The mean is "<<mean<<endl;
+   
+
+    return 0;
+}
