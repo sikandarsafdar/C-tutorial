b4e24d32f42d494c7738133aa6489e45b5b67085
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-03-30 18:51:35 +0500

    Multiplication

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+     int a,b,c;
+     cin >>a;
+     cin >>b;
+     cin >>c;
+     int multiply;
+     multiply = a*b*c;
+     cout<<"the multiplication of a,b and c is"<<multiply<<endl;
+     system("pause");
+     return 0;
+}
