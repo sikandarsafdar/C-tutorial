57618102abaf9412102e4617c46b0b884be3bb08
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-03-31 20:16:26 +0500

    Exercise

---
+++ average.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+    int average;
+    average = (a + b) / 2;
+    cout << "The average is " << average << endl;
+    int sum;
+    sum = a + b;
+    cout << "The sum is " << sum << endl;
+    system("pause");
+    return 0;
+}
---
+++ table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    for(a =1;a<=50;a++){
+        cout<<"the value of a is "<<a*10<<endl;
+    }
+    system("pause");
+    return 0;
+}
