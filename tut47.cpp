c7a04d2c88f2eaa97b3dc40eb5d0c06edf707077
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-04-04 22:04:53 +0500

    Hybrid calculator

--- tut47.cpp
+++ tut47.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 
 /*
@@ -10,8 +11,8 @@ performs any 4 scientific operations of your choice
 and displays the result using another function
 
 Creat another class HybridCalculator and inherit it using these 2 classes
-Q1. What type of inheritence are you using?
-Q2. Which mode of inheritance are you using?
+Q1. What type of inheritence are you using?-->multiple  inheritence
+Q2. Which mode of inheritance are you using?public SimpleCalculator,public ScientificCalculator
 Q3. Create an object of HybridCalculator and display results of simple and scientific calculators.
 Q4. how is code reusability implemented?
 */
@@ -19,14 +20,14 @@ Q4. how is code reusability implemented?
 class SimpleCalculator{
     int a,b;
 public:
-void getData(){
+void getDataSimple(){
     cout<<"Enter the value of a"<<endl;
     cin>>a;
     cout<<"Enter the value of b"<<endl;
     cin>>b;
 
 }
-void performOperations(){
+void performOperationsSimple(){
     cout<<"The value of a+b :"<<a+b<<endl;
     cout<<"The value of a-b :"<<a-b<<endl;
     cout<<"The value of a*b :"<<a*b<<endl;
@@ -35,12 +36,37 @@ void performOperations(){
 };
 class  ScientificCalculator{
 
+    int a,b;
+public:
+void getDataScientific(){
+    cout<<"Enter the value of a"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b"<<endl;
+    cin>>b;
+
+}
+void performOperationsScientific(){
+    cout<<"The value of cos(a) :"<<cos(a)<<endl;
+    cout<<"The value of sin(b) :"<<sin(b)<<endl;
+    cout<<"The value of exp(a) :"<<exp(a) <<endl;
+    cout<<"The value of tan(a):"<<tan(a)<<endl;
+} 
+};
+
+class HybridCalculator : public SimpleCalculator,public ScientificCalculator{
+    
 };
 int main()
 {
-SimpleCalculator calc;
-calc.getData();
-calc.performOperations();
+// // SimpleCalculator calc;
+// ScientificCalculator calc;
+// calc.getData();
+// calc.performOperations();
+HybridCalculator calc;
+calc.getDataScientific();
+calc.performOperationsScientific();
+calc.getDataSimple();
+calc.performOperationsSimple();
 system("pause"); 
     return 0;
 }
