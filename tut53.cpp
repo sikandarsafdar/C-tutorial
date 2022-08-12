d0361ba3068ba0c25f9714e9969a08188ec84ce8
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-08-12 19:30:34 +0500

    This pointer in CPP

---
+++ tut53.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std; 
+class A{
+int a;
+public:
+A & setData(int a){
+this-> a= a;
+return *this;
+}
+void getData(){
+    cout<<"The value of a is "<<a<<endl;
+}
+
+} ;
+
+
+
+int main () {
+    // this is a keyword which is a pointer which points to the objects which invokes the member function
+
+     A a;
+     a.setData(4);
+     a.getData();
+     return 0; 
+}
