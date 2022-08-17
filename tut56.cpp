6dd710f03bf97d1aba1a25699154300bc03683cb
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-08-17 23:07:29 +0500

    Virtual function in Cpp

---
+++ tut56.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std; 
+
+class BaseClass{
+    public:
+    int var_base = 1;
+    virtual  void display(){
+        cout<<"Displaying Baseclass Variable var_base"<<var_base<<endl;
+    }
+
+};
+
+class DerivedClass:public BaseClass{
+public:
+    int var_derived = 2;
+    void display(){
+        cout<<"Displaying Baseclass Variable var_base"<<var_base<<endl;
+        cout<<"Displaying Derivedclass Variable var_derived"<<var_derived<<endl;
+    }
+
+};
+int main () {
+     
+BaseClass *base_class_pointer;
+  BaseClass obj_base;
+  DerivedClass obj_derived;
+  base_class_pointer = & obj_derived;
+  base_class_pointer->display();
+     return 0;
+}
