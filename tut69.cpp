1bb3d1d475cb45f28154fec2f40f14b75036ec43
msikandar.safdar@gmail.com <msikandar.safdar@gmail.com>
2022-08-29 15:53:42 +0500

    The C++ STL

---
+++ tut69.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std; 
+/*
+Itroduction to STL
+-> competitive Programming;
+limited time.    resize array
+-> Sort
+-> Search
+
+STL--> Standard Template Library
+Library of what?
+-->Generic classes and functions
+
+Why we use STL?
+1.reuse: well tested components
+2. Time savings!
+3. STL is used becuse it is a good idea not to reinvent the wheel
+
+
+History:::
+HP company employes
+->Alexandar
+->Mengli
+
+Components of STL-->:
+1. Containers-->the object which stores data 
+- use template classes
+2. Algorithms
+- [sorting]
+- [searching]
+- [use template functions]
+3. Iterators
+--> object that points to an element in a container
+--> handles just like pointers
+--> connects algorithms with containers 
+
+
+Example::
+[1,3,5,11...]
+iterator moves as instructed by the algorithms
+*/
+int main () {
+     
+     return 0; 
+}
